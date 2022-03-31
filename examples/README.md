04ce36661ce8a06d77c07475414bc092b53d85a9
ZackeryRSmith <zackery.smith82307@gmail.com>
2022-03-30 22:20:40 -0400

    Getting ready for migration

--- examples/README.md
+++
@@ -1,1 +0,0 @@
-Here is a folder of functions that I would've added to TerControl if I didn't want to keep it lightweight.
--- tests/t.py
+++
@@ -1,6 +0,0 @@
-from tercontrol import *
-
-tc_enter_alt_screen()
-sys.stdout.flush()
-print("Hello this is line 1\nA Second line!\nA third line!?\nA four line :O!!")
-tc_clear_partial(1, 0, 3, 5)
