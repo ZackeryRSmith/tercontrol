1fe90d21fcb1e3643dcded5667dada02fc975c12
ZackeryRSmith <zackery.smith82307@gmail.com>
2022-04-01 19:53:25 -0400

    Testing

--- tests/featureTesting.c
+++ tests/featureTesting.c
@@ -16,13 +16,15 @@ int main() {
 	return 0;
 	*/
 	
+	/*
 	tc_enter_alt_screen();
 	puts("Line 1");
 	puts("Line 2");
 	puts("Line 3");
 	puts("Line 4");
 	tc_clear_partial(1, 1, 0, 0);
-	
+	*/
+
 	//printf("%sHello in cyan!%s\n", tc_color_id(69,1), TC_NRM);
 
 	/*
@@ -30,4 +32,6 @@ int main() {
 	tc_get_cols_rows(&cols, &rows);
 	printf("(%d, %d)\n", cols, rows);
 	*/
+
+	printf("%c\n", tc_getch());
 }
