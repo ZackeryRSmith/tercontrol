d4d6f605ee8dd5668beed80c79e71212907c5ba6
ZackeryRSmith <zackery.smith82307@gmail.com>
2022-04-01 15:40:12 -0400

    testing

--- tests/featureTesting.c
+++ tests/featureTesting.c
@@ -3,21 +3,31 @@
 
 int main() {
 	/*
+	tc_set_cursor(5, 5);
+	puts("a");
+	tc_set_cursor(5, 5);
+	puts(" ");
+	*/
+
+	/*
 	int X, Y = 0;
 	tc_get_cursor(&X, &Y);
 	printf("(%d, %d)\n", X, Y);
 	return 0;
 	*/
 	
-	/*
 	tc_enter_alt_screen();
-	printf("Hello from line 1\nHello from line 2\nHello from line 3\nHello from line 4\n");
-	tc_clear_partial(0, 0, 3, 3);
-	*/
+	puts("Line 1");
+	puts("Line 2");
+	puts("Line 3");
+	puts("Line 4");
+	tc_clear_partial(1, 1, 0, 0);
 	
 	//printf("%sHello in cyan!%s\n", tc_color_id(69,1), TC_NRM);
 
+	/*
 	int cols, rows;
 	tc_get_cols_rows(&cols, &rows);
 	printf("(%d, %d)\n", cols, rows);
+	*/
 }
